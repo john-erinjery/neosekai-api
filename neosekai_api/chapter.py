b5df5e891c0c187c6c913b43bd0195bac5fd8d91
john-erinjery <jancyvinod415@gmail.com>
2023-05-06 08:16:03 +0530

    optimize performance by scraping for ch details

--- neosekai_api/chapter.py
+++ neosekai_api/chapter.py
@@ -15,7 +15,7 @@ class NovelChapter:
         self.url = _url
         self.__response_object = requests.get(self.url, timeout=10)
 
-    def chapter_details(self):
+    def chapter_details(self, name_only=False):
         """
         returns chapter details : 
 
@@ -25,15 +25,22 @@ class NovelChapter:
         - chapter release date
 
         In the given order in JSON format
+        - If option name_only is set to True, the function will only return the name of the chapter
+        - This makes the program much faster as it has to go through a bunch of processes in order to get the other details 
         """
-        novel_url_ = self.url.split('neosekaitranslations.com/novel/')[-1]
-        novel_name = novel_url_[:novel_url_.index('/')]
-        novel_url = f"https://www.neosekaitranslations.com/novel/{novel_name}/"
-        novel = Novel(novel_url)
-        index_page = novel.get_index_page()
-        for i in index_page:
-            if index_page[i]['url'] == self.url:
-                return index_page[i]
+        if not name_only:
+            novel_url_ = self.url.split('neosekaitranslations.com/novel/')[-1]
+            novel_name = novel_url_[:novel_url_.index('/')]
+            novel_url = f"https://www.neosekaitranslations.com/novel/{novel_name}/"
+            novel = Novel(novel_url)
+            index_page = novel.get_index_page()
+            for i in index_page:
+                if index_page[i]['url'] == self.url:
+                    return index_page[i]
+        else:
+            soup = BeautifulSoup(self.__response_object.content, 'lxml')
+            heading = soup.find('li', attrs={'class': ['active']})
+            return heading.text.strip()
 
     def get_chapter_content(self, fancy=True):
         """
@@ -68,3 +75,6 @@ class NovelChapter:
             else:
                 continue
         return content
+
+
+print(NovelChapter('https://www.neosekaitranslations.com/novel/transfer-student/chapter-114/').chapter_details(name_only=True))
